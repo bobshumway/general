33bd0d418743a19ec407961e3e8cb416087c3eec
bobshumway <56201134+bobshumway@users.noreply.github.com>
2019-10-07 17:33:00 -0500

    Add files via upload

---
+++ Recoil.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+    public float recoil = 0.0f;
+    public float maxRecoil_x = -20f;
+    public float maxRecoil_y = 20f;
+    public float recoilSpeedUp = 2f;
+    public bool _is_recoiling;
+    public float recoilSpeedDown = 1f;
+    public int recoil_dir = 1;
+    public float temp_recoil_x;
+    public void StartRecoil(float recoilParam, float maxRecoil_xParam, float recoilSpeedParam)
+    {
+        // in seconds
+        recoil = recoilParam;
+        maxRecoil_x = maxRecoil_xParam;
+        recoilSpeedUp = recoilSpeedParam;
+        maxRecoil_y = Random.Range(-maxRecoil_xParam, maxRecoil_xParam);
+    }
+
+    void recoiling()
+    {
+        if (recoil > 0f)
+        {
+            if(recoil_dir == 1)
+            {
+                temp_recoil_x = maxRecoil_x;
+            }
+            else
+            {
+                temp_recoil_x = Mathf.Abs(maxRecoil_x);
+            }
+            //Quaternion maxRecoil = Quaternion.Euler(-maxRecoil_y, 0f, maxRecoil_x);
+            Quaternion maxRecoil = Quaternion.Euler(-maxRecoil_y, 0f, temp_recoil_x);
+            // Dampen towards the target rotation
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, maxRecoil, Time.deltaTime * recoilSpeedUp);
+            _is_recoiling = true;
+            recoil -= Time.deltaTime;
+        }
+        else
+        {
+            _is_recoiling = false;
+            recoil = 0f;
+            // Dampen towards the target rotation
+            //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, Time.deltaTime * recoilSpeedDown / 2);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, Time.deltaTime * recoilSpeedDown);
+        }
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            recoil += 0.015f;
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            if (recoil_dir == 1)
+            {
+                recoil_dir = 2;
+            }
+            else
+            {
+                recoil_dir = 1;
+            }
+        }
+        recoiling();
+    }
+}
---
+++ WeaponOverheat.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponOverheat : MonoBehaviour
+{
+    public float curr_weapon_temp;
+    public float max_weapon_temp;
+    public float _over_heat_rate;
+    public float _cool_down_rate;
+    public bool _shooting;
+    public bool _over_heated;
+    public bool _can_shoot;
+    public bool _can_animate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Overheat();
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            _shooting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            _shooting = false;
+        }
+    }
+    public void LateUpdate()
+    {
+        if(_over_heated == true)
+        {
+            StartCoroutine(waiter());
+        }
+    }
+    public void Overheat()
+    {
+        if (_shooting == true && _over_heated == false)
+        {
+            if(curr_weapon_temp <= max_weapon_temp)
+            {
+                if(_can_shoot == true)
+                {
+                    curr_weapon_temp += _over_heat_rate * Time.deltaTime;
+                }           
+            }
+            else
+            {
+                //Debug.Break();
+                //StartCoroutine(waiter());
+                _can_shoot = false;
+                _over_heated = true;
+            }
+        }
+        else
+        {
+            if(curr_weapon_temp < max_weapon_temp)
+            {
+                if(curr_weapon_temp > 0)
+                {
+                    curr_weapon_temp -= _cool_down_rate * Time.deltaTime;
+                }
+                else
+                {
+                    curr_weapon_temp = 0.0f;
+                    if (_over_heated)
+                    {
+                        //StartCoroutine(waiter());
+                        _over_heated = true;
+                    }
+                    _can_shoot = true;
+                    _over_heated = false;
+                }
+            }
+            else
+            {
+                if(curr_weapon_temp >= max_weapon_temp)
+                {
+                    curr_weapon_temp -= _cool_down_rate * Time.deltaTime;
+                }
+                else
+                {
+                    curr_weapon_temp = 0.0f;
+                    //StartCoroutine(waiter());
+                    _can_shoot = true;
+                    _over_heated = false;
+                }
+            }
+        }
+    }
+    
+    public IEnumerator waiter()
+    {
+        _can_animate = true;
+        yield return new WaitForSeconds(10f);
+        _can_shoot = true;
+        _over_heated = false;
+        _can_animate = false;
+    }
+}
